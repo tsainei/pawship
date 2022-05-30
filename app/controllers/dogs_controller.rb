5101cfb682acff6b5a4c861d3f87b6217019f0ae
tsainei yuan <hi@tsainei.com>
2022-05-30 22:53:11 +0200

    create DogsController

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,19 @@
+class DogsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'dogs/new'
+  get 'dogs/create'
+  get 'dogs/update'
+  get 'dogs/edit'
+  get 'dogs/index'
+  get 'dogs/show'
   devise_for :users
   root to: 'pages#home'
+  resources :dogs do
+    
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
