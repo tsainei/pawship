9fb8738a196b152929a9d7e816cb170df1007934
kerry pang <kerrypang15@gmail.com>
2022-05-30 17:17:00 +0200

    Remove pundit in the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit
-
-  after_action :verify_authorized, except: %i[index], unless: :skip_pundit?
-  after_action :verify_policy_scoped,
-    only: %i[index],
-    unless: :skip_pundit?
-
-  private
-
-  def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
-  end
-
 end
