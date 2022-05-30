1ee2a550ac8232bf63b243e16dad5fcc8a673c7f
tsainei yuan <hi@tsainei.com>
2022-05-30 23:25:13 +0200

    Create dogs controller and its index action and swipes controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,19 +1,19 @@
 class DogsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
   def index
+    @dog =
+      Dog
+        .where.not(id: current_user.swipes.select('dog_id'))
+        .order('RANDOM()')
+        .first
   end
 
-  def show
-  end
+  def new; end
+
+  def create; end
+
+  def update; end
+
+  def edit; end
+
+  def show; end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,4 @@
 class Dog < ApplicationRecord
   belongs_to :user
+  has_many :swipes
 end
