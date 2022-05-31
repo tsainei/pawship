e781fd4c1f15a9951fcb83189a88d55ff84ea238
tsainei yuan <hi@tsainei.com>
2022-05-31 11:25:30 +0200

    try to fix relations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,12 @@
 class Dog < ApplicationRecord
   belongs_to :user
-  has_many :swiper_dog_ids, class_name: "Swipe"
-  has_many :swiped_dog_ids, class_name: "Swipe"
+  has_many :swiper_dog,
+           class_name: 'Swipe',
+           foreign_key: 'swiper_dog_id',
+           dependent: :destroy
+  has_many :swiped_dog,
+           class_name: 'Swipe',
+           foreign_key: 'swiped_dog_id',
+           dependent: :destroy
+  validates :short_description, presence: true, length: { maximum: 50 }
 end
