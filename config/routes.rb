957ac0074e3cb95b0e586272c2400de02a067136
rosanna montanaro <rosmontanaro28@gmail.com>
2022-05-31 18:01:28 +0200

    fixed the sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'pages#home'
+  resources :dogs
   get 'swipes/new'
   get 'swipes/create'
   get 'swipes/index'
   get 'swipes/show'
-  devise_for :users
-  root to: 'pages#home'
-  resources :dogs
 #  resources :swipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
