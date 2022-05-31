fe01e3a643fe10be055fb4f7e1c3252c18b144df
tsainei yuan <hi@tsainei.com>
2022-05-31 23:49:23 +0200

    edit swipe controller and add turbo gem

--- app/controllers/swipes_controller.rb
+++ app/controllers/swipes_controller.rb
@@ -1,13 +1,22 @@
 class SwipesController < ApplicationController
-  def new
-  end
+
+  def new; end
 
   def create
+    swipe = Swipe.new
+    swipe.swiper_dog_id = current_user.dog.id
+    swipe.swiped_dog_id = params[:swiped_dog_id]
+    swipe.liked = params[:liked]
+    swipe.save!
+    if swipe.liked && swipe.swiped_dog.swipes.where(liked: true, swiped_dog_id: current_user.dog.id).any?
+      redirect_to dogs_path, notice: "It's a match"
+    else
+      redirect_to dogs_path
+    end
+
   end
 
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 end
