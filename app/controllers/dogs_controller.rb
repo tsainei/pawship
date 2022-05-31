88ababf8d33c649b667f548c05c363d2541be060
kerry pang <kerrypang15@gmail.com>
2022-05-31 12:08:46 +0200

    User can create dog profile

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,7 @@
 class DogsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index ]
+  before_action :set_dog, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @dog =
       Dog
@@ -8,13 +10,35 @@ class DogsController < ApplicationController
         .first
   end
 
-  def new; end
+  def new
+    @dog = Dog.new
+  end
 
-  def create; end
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+
+    if @dog.save
+      redirect_to dogs_path,
+                  notice: 'Your dog profile was successfully created.'
+    else
+      render :new
+    end
+  end
 
   def update; end
 
   def edit; end
 
   def show; end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :gender, :personality, :sex_orientation, :training_status, :breed, :has_breed_certificate, :hobbies, :address, :short_description)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,5 @@ class Dog < ApplicationRecord
   belongs_to :user
   has_many :swiper_dog_ids, class_name: "Swipe"
   has_many :swiped_dog_ids, class_name: "Swipe"
+  validates :name, :age, :gender, presence: true
 end
