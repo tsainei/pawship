25954c2917bc082d81ebbe6f85653f0f4f7dd987
Kerryyee <kerrypang15@gmail.com>
2022-06-01 16:35:14 +0200

    Update dog.rb

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,8 +5,17 @@ class Dog < ApplicationRecord
   has_many :swipers, class_name: 'Swipe', foreign_key: 'swiped_dog_id'
 
   validates :short_description, presence: true, length: { maximum: 50 }
-
-
+  validates :name,
+            :birthday,
+            :personality,
+            :gender,
+            :sex_orientation,
+            :training_status,
+            :breed,
+            :has_breed_certificate,
+            :hobbies,
+            :address,
+            presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
