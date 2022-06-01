c03e961a3b207f24d41ba7b8bb2e8691b50344a3
rosanna montanaro <rosmontanaro28@gmail.com>
2022-06-01 11:04:13 +0200

    added the lat and log to the Dog table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_190533) do
+ActiveRecord::Schema.define(version: 2022_06_01_090217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2022_05_31_190533) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "short_description"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
