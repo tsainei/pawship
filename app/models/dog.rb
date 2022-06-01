d432d32db689d7c2862f4251708e5b7c9e8601ee
tsainei yuan <hi@tsainei.com>
2022-06-01 16:31:03 +0200

    fix dog model and remove age from validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -6,7 +6,7 @@ class Dog < ApplicationRecord
 
   validates :short_description, presence: true, length: { maximum: 50 }
   validates :name,
-            :age,
+            :birthday,
             :personality,
             :gender,
             :sex_orientation,
@@ -15,5 +15,14 @@ class Dog < ApplicationRecord
             :has_breed_certificate,
             :hobbies,
             :address,
+            :photo,
             presence: true
+  validate :dates_valid?
+
+  def dates_valid?
+    return if birthday.blank? || birthday.future?
+    if birthday > Time.current.beginning_of_day
+      return errors.add(:birthday, 'must be in the future')
+    end
+  end
 end
