6df13189beace18e30de39fd78e1049e9c86d9ca
rosanna montanaro <rosmontanaro28@gmail.com>
2022-06-01 12:15:09 +0200

    installed the mapbox javascript controller and set the method as well as the view into the show page

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -15,4 +15,7 @@ class Dog < ApplicationRecord
             :hobbies,
             :address,
             presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
