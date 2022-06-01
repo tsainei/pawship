5061a29704a02f6c8cd93d6737442a08c2b6c430
rosanna montanaro <rosmontanaro28@gmail.com>
2022-06-01 16:48:45 +0200

    Restore the normal style

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/rosmontanaro/cl3vclm6v002c14o2z7i6aa4k"
+      style: "mapbox://styles/mapbox/streets-v10"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
