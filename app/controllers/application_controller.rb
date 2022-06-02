1cf34e97dd2e4ccb83e274a8f4fa8d1c8984de6c
rosmontanaro <101641769+rosmontanaro@users.noreply.github.com>
2022-06-02 11:55:39 +0200

    New user path (#36)
    * Fixed user path from login
    
    * Fixed user path from login_now it works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(resource)
+
+    if resource == current_user.dog.nil?
+      new_dog_path
+    else
+      dogs_path
+    end
+  end
 end
