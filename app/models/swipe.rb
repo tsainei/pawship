1f89adcc594730b2a6c42668cc4a79330f9c1563
tsainei yuan <hi@tsainei.com>
2022-06-02 09:39:55 +0200

    fix stuff

--- app/models/swipe.rb
+++ app/models/swipe.rb
@@ -1,4 +1,5 @@
 class Swipe < ApplicationRecord
   belongs_to :swiper_dog, class_name: 'Dog', foreign_key: 'swiper_dog_id'
   belongs_to :swiped_dog, class_name: 'Dog', foreign_key: 'swiped_dog_id'
+  scope :likes, -> { where(liked: true) }
 end
