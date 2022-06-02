46153ab370264c7a468bc9fb0ca6c36c026c0d75
rosanna montanaro <rosmontanaro28@gmail.com>
2022-06-02 11:08:51 +0200

    Fixed user path from login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if current_user.dog.nil?
+      redirect_to new_dog_path
+    else
+      redirect_to dogs_path
+    end
   end
+
 end
