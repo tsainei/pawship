7f144377021c5cfd62cf4b6a6be1c2d6041afddd
rosanna montanaro <rosmontanaro28@gmail.com>
2022-06-07 23:19:10 +0200

    popup done

--- app/javascript/controllers/popup_controller.js
+++ app/javascript/controllers/popup_controller.js
@@ -1,26 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['icon', 'description'];
+  static targets = ['icon', 'description', 'detail'];
 
   connect() {
-
     const box = this.iconTarget
-    box.addEventListener("click", (event) =>{
-      this.descriptionTarget.classList.toggle("red");
+    box.addEventListener("click", () => {
+      this.descriptionTarget.classList.toggle('details');
       console.log(this.descriptionTarget);
     })
-  }
-
-  disable() {
-    this.buttonTarget.innerText = 'Bingo!';
-    this.buttonTarget.setAttribute('disabled', '');
-    this.linkTarget.classList.remove('d-none');
-  }
-
-  reset() {
-    this.buttonTarget.innerText = 'Click me!';
-    this.buttonTarget.removeAttribute('disabled');
-    this.linkTarget.classList.add('d-none');
   }
 }
